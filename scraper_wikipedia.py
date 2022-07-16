0267f90b2e3003b07a41ceabd67ac12eaa493492
Lucas Ketzer <lucasketzerdosreis@gmail.com>
2022-07-16 02:33:05 -0300

    feat: adding initial draft for wikipedia page scraper

---
+++ scraper_wikipedia.py
@@ -0,0 +1,5 @@
+# importing libraries
+import requests
+
+# requesting the discography page
+discography_page = requests.get("https://pt.wikipedia.org/wiki/Discografia_do_Grupo_Especial_do_Rio_de_Janeiro")
